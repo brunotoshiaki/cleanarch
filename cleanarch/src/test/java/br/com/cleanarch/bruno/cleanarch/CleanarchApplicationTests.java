3a89a6410fe4493b19e6a4de469c9b931211b173
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2022-11-23 08:59:39 -0300

    Criação do projeto

---
+++ cleanarch/src/test/java/br/com/cleanarch/bruno/cleanarch/CleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleanarch.bruno.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
